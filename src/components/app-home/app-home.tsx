1a3f3f58c94419f38a4507186f7b9982474a7b0e
dmartinjs <53537199+dmartinjs@users.noreply.github.com>
2020-03-08 22:11:29 +0100

    Add Oxanium Fonts

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -8,7 +8,18 @@ export class AppHome {
 
   render() {
     return [
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>F1rst</ion-title>
+        </ion-toolbar>
+      </ion-header>,
+
       <ion-content>
+        <ion-header collapse="condense">
+          <ion-toolbar>
+            <ion-title size="large">F1rst</ion-title>
+          </ion-toolbar>
+        </ion-header>
         <next-race></next-race>
       </ion-content>
     ];
