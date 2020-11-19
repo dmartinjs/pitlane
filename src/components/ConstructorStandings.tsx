21d1cd28631a28b69d8079dd55653c26d39d5be4
dmartinjs <53537199+dmartinjs@users.noreply.github.com>
2020-11-19 18:58:04 +0100

    feat(ConstructorStandings): add drivers of constructors

--- src/components/ConstructorStandings.tsx
+++ src/components/ConstructorStandings.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { IonList, IonItem, IonLabel, IonBadge, IonSkeletonText } from '@ionic/react';
 import { useHistory } from 'react-router';
-import { ConstructorStanding } from '../models';
+import { ConstructorStanding, DriverStanding } from '../models';
 
 const ConstructorStandings: React.FC = () => {
   let history = useHistory();
   const [constructors, setConstructors] = useState<[ConstructorStanding] | null>(null);
+  const [drivers, setDrivers] = useState<[DriverStanding] | null>(null);
 
   useEffect(() => {
+    fetch('https://ergast.com/api/f1/current/driverStandings.json')
+      .then(res => res.json())
+      .then(result => setDrivers(result.MRData.StandingsTable.StandingsLists[0].DriverStandings));
+
     fetch('https://ergast.com/api/f1/current/constructorStandings.json')
       .then(res => res.json())
       .then(result => setConstructors(result.MRData.StandingsTable.StandingsLists[0].ConstructorStandings));
@@ -17,7 +22,7 @@ const ConstructorStandings: React.FC = () => {
     history.push(`/constructor/${constructorId}`);
   }
 
-  if (constructors === null) {
+  if (constructors === null || drivers === null) {
     return (
       <IonList>
         {[...Array(8)].map((index) =>
@@ -25,7 +30,7 @@ const ConstructorStandings: React.FC = () => {
             <div slot="start">&nbsp;&nbsp;</div>
             <IonLabel>
               <IonSkeletonText animated style={{ height: '16px', width: '40%' }}/>
-              <IonSkeletonText animated style={{ height: '16px', width: '30%' }}/>
+              <IonSkeletonText animated style={{ height: '16px', width: '60%' }}/>
             </IonLabel>
             <IonSkeletonText slot="end" animated style={{ height: '16px', width: '58px' }}/>
           </IonItem>
@@ -41,8 +46,17 @@ const ConstructorStandings: React.FC = () => {
           {constructor.position}
         </div>
         <IonLabel>
-          <h3>{constructor.Constructor.name}</h3>
-          <p>{constructor.Constructor.nationality}</p>
+          <h3><strong className="ion-text-uppercase">{constructor.Constructor.name}</strong></h3>
+          <p>
+          {drivers && drivers
+            .filter(driver => driver.Constructors[0].constructorId === constructor.Constructor.constructorId)
+            .slice(0, 2)
+            .map<React.ReactNode>(driver => (
+              driver.Driver.familyName 
+            ))
+            .reduce((prev, curr) => [prev, ' / ', curr])
+          }
+          </p>
         </IonLabel>
         <IonBadge color="medium" slot="end">{constructor.points} PTS</IonBadge>
       </IonItem>
