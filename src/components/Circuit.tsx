d2e1279d86073518e24ff679fa05ef0110087ef0
dmartinjs <53537199+dmartinjs@users.noreply.github.com>
2021-02-24 23:01:16 +0100

    fix(theme): fix theme colors

--- src/components/Circuit.tsx
+++ src/components/Circuit.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { IonSkeletonText, IonItem, IonAvatar, IonLabel } from '@ionic/react';
+import { IonSkeletonText, IonItem, IonLabel, IonThumbnail, IonList } from '@ionic/react';
 import { Race } from '../models';
 
 const Circuit: React.FC<{season: string, round: string, circuit: string}> = ({season, round, circuit}) => {
@@ -15,27 +15,31 @@ const Circuit: React.FC<{season: string, round: string, circuit: string}> = ({se
 
   if (race === null) {
     return (
-      <IonItem>
-        <IonAvatar slot="start">
-          &nbsp;
-        </IonAvatar>
-        <IonLabel>
-          <h2><IonSkeletonText animated style={{ height: '11px', width: '70px' }}/></h2>
-          <p><IonSkeletonText animated style={{ height: '11px', width: '120px' }}/></p>
-        </IonLabel>
-      </IonItem>
+      <IonList>
+        <IonItem lines="none">
+          <IonThumbnail slot="start" className="circuit-country-thumbnail ion-margin-end">
+            &nbsp;
+          </IonThumbnail>
+          <IonLabel>
+            <h2><IonSkeletonText animated style={{ height: '11px', width: '70px' }}/></h2>
+            <p><IonSkeletonText animated style={{ height: '11px', width: '120px' }}/></p>
+          </IonLabel>
+        </IonItem>
+      </IonList>
     );
   }
   return (
-    <IonItem>
-      <IonAvatar slot="start">
-        <img src={`assets/img/flags/${race.Circuit.Location.country}.svg`} alt={race.Circuit.Location.country}/>
-      </IonAvatar>
-      <IonLabel>
-        <h2><strong>{race.Circuit.Location.country}</strong> {season}</h2>
-        <p>{race.Circuit.circuitName}</p>
-      </IonLabel>
-    </IonItem>
+    <IonList>
+      <IonItem lines="none">
+        <IonThumbnail slot="start" className="circuit-country-thumbnail ion-margin-end">
+          <img src={`assets/img/flags/${race.Circuit.Location.country}.svg`} alt={race.Circuit.Location.country}/>
+        </IonThumbnail>
+        <IonLabel>
+          <h2>{race.Circuit.circuitName}</h2>
+          <p>{race.Circuit.Location.country}</p>
+        </IonLabel>
+      </IonItem>
+    </IonList>
   );
 };
 
