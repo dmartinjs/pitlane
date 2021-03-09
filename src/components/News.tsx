c9a54365de127ae55562215343e40bd222c5997c
dmartinjs <53537199+dmartinjs@users.noreply.github.com>
2021-03-09 22:57:04 +0100

    reafactor(News): add img skeleton

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -26,7 +26,7 @@ const News: React.FC = () => {
       {feed && feed.map((item: any, index: any) =>
         <IonItem key={index} button href={item.link.replace(/utm_[^&]+&?/g, '')} rel="noopener" target="_blank">
           <IonThumbnail slot="start">
-            <IonImg src={item.enclosure.link} alt={item.title}/>
+            <IonImg className="img" src={item.enclosure.link} alt={item.title}/>
           </IonThumbnail>
           <IonLabel className="ion-text-wrap ion-margin-end">
             {item.title}
