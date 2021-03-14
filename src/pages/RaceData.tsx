bc1b1ce5f0c243471e711a02ff67bae22189187b
dmartinjs <53537199+dmartinjs@users.noreply.github.com>
2021-03-14 13:20:34 +0100

    feat(raceData): add tabs

--- src/pages/RaceData.tsx
+++ src/pages/RaceData.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/react';
+import React, { useRef, useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton, IonSegment, IonSegmentButton, IonLabel, IonSlides, IonSlide, IonGrid, IonRow, IonCol } from '@ionic/react';
 import { RouteComponentProps } from 'react-router';
+import { slideOptions } from '../utils/SlideOptions';
 
 interface RaceDataProps extends RouteComponentProps<{
   season: string,
@@ -9,6 +10,35 @@ interface RaceDataProps extends RouteComponentProps<{
 }> {}
 
 const RaceData: React.FC<RaceDataProps> = ({match}) => {
+  const [selectedSegment, SetSelectedSegment] = useState<string>('positions');
+
+  const slider = useRef<HTMLIonSlidesElement>(null);
+
+  const onSegmentChange = (event: CustomEvent) => {
+    SetSelectedSegment(event.detail.value);
+
+    switch(event.detail.value) {
+      case 'positions':
+        slider.current!.slideTo(0);
+        break;
+      case 'laptimes':
+        slider.current!.slideTo(1);
+        break;
+    }
+  }
+
+  const onSlideChange = (event: any) => {
+    event.target.getActiveIndex().then((value: any) => {
+      switch(value) {
+        case 0:
+          SetSelectedSegment('positions');
+          break;
+        case 1:
+          SetSelectedSegment('laptimes');
+          break;
+      }
+    })
+  }
 
   return (
     <IonPage>
@@ -19,8 +49,38 @@ const RaceData: React.FC<RaceDataProps> = ({match}) => {
           </IonButtons>
           <IonTitle>Race Data</IonTitle>
         </IonToolbar>
+        <IonToolbar>
+          <IonSegment onIonChange={onSegmentChange} value={selectedSegment} scrollable>
+            <IonSegmentButton value="positions">
+              <IonLabel>Positions</IonLabel>
+            </IonSegmentButton>
+            <IonSegmentButton value="laptimes">
+              <IonLabel>Lap Times</IonLabel>
+            </IonSegmentButton>
+          </IonSegment>
+        </IonToolbar>
       </IonHeader>
       <IonContent>
+        <IonSlides onIonSlideDidChange={onSlideChange} ref={slider} options={slideOptions}>
+          <IonSlide>
+            <IonGrid>
+              <IonRow>
+                <IonCol>
+                  Positions
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonSlide>
+          <IonSlide>
+            <IonGrid>
+              <IonRow>
+                <IonCol>
+                  Lap Times
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonSlide>
+        </IonSlides>
       </IonContent>
     </IonPage>
   );
