012f4d6fd5b4ed8d2d83a0b48b959874939ddd5b
dmartinjs <53537199+dmartinjs@users.noreply.github.com>
2021-03-18 23:33:42 +0100

    refactor(Positions): add list of drivers and their position below chart

--- src/components/race/data/Positons.tsx
+++ src/components/race/data/Positons.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import {  } from '@ionic/react';
+import { IonItem, IonLabel, IonList } from '@ionic/react';
 import { Lap } from '../../../models';
 import { CartesianGrid, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
 const Positions: React.FC<{season: string, round: string, driverId: string}> = ({season, round, driverId}) => {
-  const [results, setResults] = useState<any[] | null>(null);
+  const [results, setResults] = useState<[Lap] | null>(null);
 
   useEffect(() => {
     fetch(`https://ergast.com/api/f1/${season}/${round}/laps.json?limit=1000`)
       .then(res => res.json())
-      .then(result => setResults(getData(result.MRData.RaceTable.Races[0].Laps)));
+      .then(result => setResults(result.MRData.RaceTable.Races[0].Laps));
   }, [season, round]);
 
   const getData = (positions: [Lap]) => {
@@ -30,33 +30,55 @@ const Positions: React.FC<{season: string, round: string, driverId: string}> = (
     );
   }
   return (
-    <ResponsiveContainer height={400}>
-      <LineChart width={400} height={450} data={results}>
-        <CartesianGrid horizontal={false} stroke="var(--ion-border-color)"/>
-        <XAxis dataKey="lap" interval={8} tickLine={false}/>
-        <YAxis domain={[0, 20]} orientation="right" reversed={true} hide={true} />
-        <Line type="monotone" dataKey="hamilton" stroke="#00D2BE" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="bottas" stroke="#00D2BE" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="max_verstappen" stroke="#1E41FF" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="albon" stroke="#1E41FF" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="leclerc" stroke="#DC0000" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="vettel" stroke="#DC0000" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="sainz" stroke="#FF8700" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="norris" stroke="#FF8700" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="perez" stroke="#F596C8" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="stroll" stroke="#F596C8" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="ricciardo" stroke="#FFF500" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="ocon" stroke="#FFF500" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="gasly" stroke="#FFFFFF" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="kvyat" stroke="#FFFFFF" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="raikkonen" stroke="#9B0000" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="giovinazzi" stroke="#9B0000" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="kevin_magnussen" stroke="#787878" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="grosjean" stroke="#787878" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="russell" stroke="#0082fa" dot={false} strokeWidth={2} />
-        <Line type="monotone" dataKey="latifi" stroke="#0082fa" dot={false} strokeWidth={2} />
-      </LineChart>
-    </ResponsiveContainer>
+    <>
+      <ResponsiveContainer height={400}>
+        <LineChart width={400} height={450} data={getData(results)}>
+          <CartesianGrid horizontal={false} stroke="var(--ion-border-color)"/>
+          <XAxis dataKey="lap" interval={8} tickLine={false}/>
+          <YAxis domain={[0, 20]} orientation="right" reversed={true} hide={true} />
+          <Line type="monotone" dataKey="hamilton" stroke="#00D2BE" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="bottas" stroke="#00D2BE" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="max_verstappen" stroke="#1E41FF" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="albon" stroke="#1E41FF" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="leclerc" stroke="#DC0000" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="vettel" stroke="#DC0000" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="sainz" stroke="#FF8700" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="norris" stroke="#FF8700" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="perez" stroke="#F596C8" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="stroll" stroke="#F596C8" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="ricciardo" stroke="#FFF500" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="ocon" stroke="#FFF500" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="gasly" stroke="#FFFFFF" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="kvyat" stroke="#FFFFFF" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="raikkonen" stroke="#9B0000" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="giovinazzi" stroke="#9B0000" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="kevin_magnussen" stroke="#787878" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="grosjean" stroke="#787878" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="russell" stroke="#0082fa" dot={false} strokeWidth={2} />
+          <Line type="monotone" dataKey="latifi" stroke="#0082fa" dot={false} strokeWidth={2} />
+        </LineChart>
+      </ResponsiveContainer>
+      <IonList lines="full">
+        <IonItem>
+          <div className="race-position ion-text-center ion-margin-end">
+            P
+          </div>
+          <IonLabel>
+            Driver
+          </IonLabel>
+        </IonItem>
+        {results[results.length - 1]?.Timings.map(result =>
+          <IonItem key={result.driverId}>
+            <div className="race-position ion-margin-end ion-text-center font-weight-bold">
+              {result.position}.
+            </div>
+            <IonLabel>
+              <h3 className="ion-text-uppercase font-weight-bold">{result.driverId}</h3>
+            </IonLabel>
+          </IonItem>
+        )}
+      </IonList>
+    </>
   );
 };
 
