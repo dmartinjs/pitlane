df61b6ccf67d22cd5ed60f7b667e8e3c5a3b364b
dmartinjs <53537199+dmartinjs@users.noreply.github.com>
2021-03-18 21:47:32 +0100

    feat(Positions): make the chart responsive

--- src/components/race/data/Positons.tsx
+++ src/components/race/data/Positons.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {  } from '@ionic/react';
 import { Lap } from '../../../models';
-import { Line, LineChart, XAxis, YAxis } from 'recharts';
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
 const Positions: React.FC<{season: string, round: string, driverId: string}> = ({season, round, driverId}) => {
   const [results, setResults] = useState<any[] | null>(null);
@@ -42,30 +42,32 @@ const Positions: React.FC<{season: string, round: string, driverId: string}> = (
     );
   }
   return (
-    <LineChart width={400} height={500} data={results}>
-      <XAxis dataKey="lap" interval={8}/>
-      <YAxis domain={[0, 22]} orientation="right" reversed={true} hide={true} />
-      <Line type="monotone" dataKey="hamilton" stroke="#00d2be" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="bottas" stroke="#00d2be" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="max_verstappen" stroke="#1e41ff" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="albon" stroke="#1e41ff" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="leclerc" stroke="#dc0000" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="vettel" stroke="#dc0000" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="sainz" stroke="#ff8700" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="norris" stroke="#ff8700" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="perez" stroke="#f596c8" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="stroll" stroke="#f596c8" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="ricciardo" stroke="#fff500" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="ocon" stroke="#fff500" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="gasly" stroke="#469bff" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="kvyat" stroke="#469bff" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="raikkonen" stroke="#9b0000" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="giovinazzi" stroke="#9b0000" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="kevin_magnussen" stroke="#bd9e57" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="grosjean" stroke="#bd9e57" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="russell" stroke="#ffffff" dot={false} strokeWidth={2} />
-      <Line type="monotone" dataKey="latifi" stroke="#ffffff" dot={false} strokeWidth={2} />
-    </LineChart>
+    <ResponsiveContainer height={400}>
+      <LineChart width={400} height={500} data={results}>
+        <XAxis dataKey="lap" interval={8}/>
+        <YAxis domain={[0, 22]} orientation="right" reversed={true} hide={true} />
+        <Line type="monotone" dataKey="hamilton" stroke="#00D2BE" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="bottas" stroke="#00D2BE" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="max_verstappen" stroke="#1E41FF" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="albon" stroke="#1E41FF" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="leclerc" stroke="#DC0000" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="vettel" stroke="#DC0000" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="sainz" stroke="#FF8700" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="norris" stroke="#FF8700" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="perez" stroke="#F596C8" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="stroll" stroke="#F596C8" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="ricciardo" stroke="#FFF500" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="ocon" stroke="#FFF500" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="gasly" stroke="#FFFFFF" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="kvyat" stroke="#FFFFFF" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="raikkonen" stroke="#9B0000" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="giovinazzi" stroke="#9B0000" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="kevin_magnussen" stroke="#787878" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="grosjean" stroke="#787878" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="russell" stroke="#0082fa" dot={false} strokeWidth={2} />
+        <Line type="monotone" dataKey="latifi" stroke="#0082fa" dot={false} strokeWidth={2} />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
