9ae5436d1c08fbc3da34805fb9f9066ecd0727d0
dmartinjs <53537199+dmartinjs@users.noreply.github.com>
2021-03-22 21:06:28 +0100

    feat(LapTimes): add list of laps with respective time

--- src/components/race/data/LapTimes.tsx
+++ src/components/race/data/LapTimes.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {  } from '@ionic/react';
+import { IonBadge, IonItem, IonLabel, IonList } from '@ionic/react';
 import { Lap } from '../../../models';
 import { Area, AreaChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
@@ -28,19 +28,45 @@ const LapTimes: React.FC<{season: string, round: string, driverId: string}> = ({
     );
   }
   return (
-    <ResponsiveContainer height={400}>
-      <AreaChart width={400} height={400} data={getData(results)}>
-        <defs>
-          <linearGradient id="time" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#e10600" stopOpacity={0.8}/>
-            <stop offset="95%" stopColor="#e10600" stopOpacity={0}/>
-          </linearGradient>
-        </defs>
-        <XAxis dataKey="lap" interval={8} tickLine={false}/>
-        <YAxis dataKey="time" hide={true} />
-        <Area type="monotone" dataKey="time" stroke="#e10600" fillOpacity={1} fill="url(#time)" />
-      </AreaChart>
-    </ResponsiveContainer>
+    <>
+      <ResponsiveContainer height={400}>
+        <AreaChart width={400} height={400} data={getData(results)}>
+          <defs>
+            <linearGradient id="time" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#e10600" stopOpacity={0.8}/>
+              <stop offset="95%" stopColor="#e10600" stopOpacity={0}/>
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="lap" interval={8} tickLine={false}/>
+          <YAxis dataKey="time" hide={true} />
+          <Area type="monotone" dataKey="time" stroke="#e10600" fillOpacity={1} fill="url(#time)" />
+        </AreaChart>
+      </ResponsiveContainer>
+      <IonList lines="full">
+        <IonItem>
+          <div className="race-position ion-text-center ion-margin-end">
+            Lap
+          </div>
+          <IonLabel>
+            Driver
+          </IonLabel>
+          <div className="race-time">
+            Time
+          </div>
+        </IonItem>
+        {results.map(result =>
+          <IonItem key={result.number}>
+            <div className="race-position ion-margin-end ion-text-center font-weight-bold">
+              {result.number}
+            </div>
+            <IonLabel>
+              <h3 className="ion-text-uppercase font-weight-bold">{result.Timings[0].driverId}</h3>
+            </IonLabel>
+            <IonBadge color="medium" mode="ios" className="race-time">{result.Timings[0].time}</IonBadge>
+          </IonItem>
+        )}
+      </IonList>
+    </>
   );
 };
 
