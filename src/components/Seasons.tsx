cd2c87a62c43dab6dea5f6017479d95d3b759f35
dmartinjs <53537199+dmartinjs@users.noreply.github.com>
2022-02-11 20:09:41 +0100

    fix(Seasons): handle current year not available

--- src/components/Seasons.tsx
+++ src/components/Seasons.tsx
@@ -5,22 +5,32 @@ import DriverResults from './driver/DriverResults';
 import ConstructorResults from './constructor/ConstructorResults';
 
 const Seasons: React.FC<{driverId?: string, constructorId?: string}> = ({driverId, constructorId}) => {
-  const [selectedSegment, SetSelectedSegment] = useState<string>(`${new Date().getFullYear()}`);
+  const [selectedSegment, setSelectedSegment] = useState<string>(`${new Date().getFullYear()}`);
   const [seasons, setSeasons] = useState<[Season] | null>(null);
 
-  const onChange = (event: CustomEvent) => SetSelectedSegment(event.detail.value);
+  const onChange = (event: CustomEvent) => setSelectedSegment(event.detail.value);
 
   useEffect(() => {
     if(driverId !== undefined) {
       fetch(`https://ergast.com/api/f1/drivers/${driverId}/seasons.json`)
         .then(res => res.json())
-        .then(result => setSeasons(result.MRData.SeasonTable.Seasons.reverse()));
+        .then(result => {
+          if(result.MRData.SeasonTable.Seasons[result.MRData.SeasonTable.Seasons.length - 1].season !== new Date().getFullYear()) {
+            setSelectedSegment(result.MRData.SeasonTable.Seasons[result.MRData.SeasonTable.Seasons.length - 1].season);
+          }
+          setSeasons(result.MRData.SeasonTable.Seasons.reverse());
+        });
     }
 
     if(constructorId !== undefined) {
       fetch(`https://ergast.com/api/f1/constructors/${constructorId}/seasons.json?limit=100`)
         .then(res => res.json())
-        .then(result => setSeasons(result.MRData.SeasonTable.Seasons.reverse()));
+        .then(result => {
+          if(result.MRData.SeasonTable.Seasons[result.MRData.SeasonTable.Seasons.length - 1].season !== new Date().getFullYear()) {
+            setSelectedSegment(result.MRData.SeasonTable.Seasons[result.MRData.SeasonTable.Seasons.length - 1].season);
+          }
+          setSeasons(result.MRData.SeasonTable.Seasons.reverse())
+        });
     }
   }, [driverId, constructorId]);
 
