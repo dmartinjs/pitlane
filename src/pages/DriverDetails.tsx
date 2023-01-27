169346b18b386f8a7bf61412e353d9d88bb334af
dmartinjs <53537199+dmartinjs@users.noreply.github.com>
2023-01-28 00:22:51 +0100

    refactor(DriverDetails): update to tab layout

--- src/pages/DriverDetails.tsx
+++ src/pages/DriverDetails.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonToolbar, IonButtons, IonBackButton, IonItem, IonLabel, IonList, IonIcon, IonThumbnail, IonTitle, IonImg } from '@ionic/react';
+import React, { useEffect, useRef, useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonToolbar, IonButtons, IonBackButton, IonItem, IonLabel, IonList, IonIcon, IonThumbnail, IonTitle, IonImg, IonSlides, IonSlide, IonGrid, IonRow, IonCol, IonSegment, IonSegmentButton } from '@ionic/react';
 import { RouteComponentProps } from 'react-router';
 import { DriverStandingsLists } from '../models';
 import { giftOutline, todayOutline } from 'ionicons/icons';
 import DriverSeasons from '../components/driver/DriverSeasons';
 import DriverRacesPodiums from '../components/driver/DriverAchievements';
+import { slideOptions } from '../utils/SlideOptions';
 
 interface DriverDetailsProps extends RouteComponentProps<{
   driverId: string,
@@ -12,6 +13,41 @@ interface DriverDetailsProps extends RouteComponentProps<{
 
 const DriverDetails: React.FC<DriverDetailsProps> = ({match}) => {
   const [driver, setDriver] = useState<DriverStandingsLists | null>(null);
+  const [selectedSegment, SetSelectedSegment] = useState<string>('stats');
+
+  const slider = useRef<HTMLIonSlidesElement>(null);
+
+  const onSegmentChange = (event: CustomEvent) => {
+    SetSelectedSegment(event.detail.value);
+
+    switch(event.detail.value) {
+      case 'stats':
+        slider.current!.slideTo(0);
+        break;
+      case 'teams':
+        slider.current!.slideTo(1);
+        break;
+      case 'bio':
+        slider.current!.slideTo(2);
+        break;
+    }
+  }
+
+  const onSlideChange = (event: any) => {
+    event.target.getActiveIndex().then((value: any) => {
+      switch(value) {
+        case 0:
+          SetSelectedSegment('stats');
+          break;
+        case 1:
+          SetSelectedSegment('teams');
+          break;
+        case 2:
+          SetSelectedSegment('bio');
+          break;
+      }
+    })
+  }
 
   useEffect(() => {
     fetch(`https://ergast.com/api/f1/current/drivers/${match.params.driverId}/driverStandings.json`)
@@ -45,45 +81,77 @@ const DriverDetails: React.FC<DriverDetailsProps> = ({match}) => {
                 </IonThumbnail>
               </IonItem>
             </IonList>
-            <h3 className="ion-margin-start">Team</h3>
-            <IonItem lines="full" button routerLink={`/constructor/${driver.DriverStandings[0].Constructors[0].constructorId}`}>
-              <IonIcon lazy slot="start" size="large" className="constructor ion-margin-end" src={`assets/img/constructors/${driver.DriverStandings[0].Constructors[0].constructorId}.svg`}/>
-              <IonLabel>
-                <h2 className="font-weight-bold">{driver.DriverStandings[0].Constructors[0].name}</h2>
-              </IonLabel>
-              <IonThumbnail slot="end" className="country-thumbnail">
-                <IonImg src={`assets/img/flags/${driver.DriverStandings[0].Constructors[0].nationality}.svg`} alt={driver.DriverStandings[0].Driver.nationality}/>
-              </IonThumbnail>
-            </IonItem>
-
-            <h3 className="ion-margin-start">Achievements</h3>
-            <DriverSeasons driverId={driver.DriverStandings[0].Driver.driverId}/>
-            <DriverRacesPodiums driverId={driver.DriverStandings[0].Driver.driverId}/>
-
-            <h3 className="ion-margin-start">Personal Informations</h3>
-            <IonItem lines="full">
-              <IonThumbnail slot="start" className="country-thumbnail ion-margin-end">
-                <IonImg src={`assets/img/flags/${driver.DriverStandings[0].Driver.nationality}.svg`} alt={driver.DriverStandings[0].Driver.nationality}/>
-              </IonThumbnail>
-              <IonLabel>
-                <p>Nationality</p>
-                <h2 className="font-weight-bold">{driver.DriverStandings[0].Driver.nationality}</h2>
-              </IonLabel>
-            </IonItem>
-            <IonItem lines="full">
-              <IonIcon slot="start" className="ion-margin-end" icon={giftOutline}></IonIcon>
-              <IonLabel>
-                <p>Age</p>
-                <h2 className="font-weight-bold">{new Date().getFullYear() - new Date(driver.DriverStandings[0].Driver.dateOfBirth).getFullYear()}</h2>
-              </IonLabel>
-            </IonItem>
-            <IonItem lines="full">
-              <IonIcon slot="start" className="ion-margin-end" icon={todayOutline}></IonIcon>
-              <IonLabel>
-                <p>Date of birth</p>
-                <h2 className="font-weight-bold">{new Intl.DateTimeFormat('en-GB').format(new Date(driver.DriverStandings[0].Driver.dateOfBirth))}</h2>
-              </IonLabel>
-            </IonItem>
+            <IonSegment onIonChange={onSegmentChange} value={selectedSegment}>
+              <IonSegmentButton value="stats">
+                <IonLabel>Stats</IonLabel>
+              </IonSegmentButton>
+              <IonSegmentButton value="teams">
+                <IonLabel>Teams</IonLabel>
+              </IonSegmentButton>
+              <IonSegmentButton value="bio">
+                <IonLabel>Bio</IonLabel>
+              </IonSegmentButton>
+            </IonSegment>
+            <IonSlides onIonSlideDidChange={onSlideChange} ref={slider} options={slideOptions}>
+              <IonSlide>
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <DriverSeasons driverId={driver.DriverStandings[0].Driver.driverId}/>
+                      <DriverRacesPodiums driverId={driver.DriverStandings[0].Driver.driverId}/>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonSlide>
+              <IonSlide>
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonItem lines="full" button routerLink={`/constructor/${driver.DriverStandings[0].Constructors[0].constructorId}`}>
+                        <IonIcon lazy slot="start" size="large" className="constructor ion-margin-end" src={`assets/img/constructors/${driver.DriverStandings[0].Constructors[0].constructorId}.svg`}/>
+                        <IonLabel>
+                          <h2 className="font-weight-bold">{driver.DriverStandings[0].Constructors[0].name}</h2>
+                        </IonLabel>
+                        <IonThumbnail slot="end" className="country-thumbnail">
+                          <IonImg src={`assets/img/flags/${driver.DriverStandings[0].Constructors[0].nationality}.svg`} alt={driver.DriverStandings[0].Driver.nationality}/>
+                        </IonThumbnail>
+                      </IonItem>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonSlide>
+              <IonSlide>
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <IonItem lines="full">
+                        <IonThumbnail slot="start" className="country-thumbnail ion-margin-end">
+                          <IonImg src={`assets/img/flags/${driver.DriverStandings[0].Driver.nationality}.svg`} alt={driver.DriverStandings[0].Driver.nationality}/>
+                        </IonThumbnail>
+                        <IonLabel>
+                          <p>Nationality</p>
+                          <h2 className="font-weight-bold">{driver.DriverStandings[0].Driver.nationality}</h2>
+                        </IonLabel>
+                      </IonItem>
+                      <IonItem lines="full">
+                        <IonIcon slot="start" className="ion-margin-end" icon={giftOutline}></IonIcon>
+                        <IonLabel>
+                          <p>Age</p>
+                          <h2 className="font-weight-bold">{new Date().getFullYear() - new Date(driver.DriverStandings[0].Driver.dateOfBirth).getFullYear()}</h2>
+                        </IonLabel>
+                      </IonItem>
+                      <IonItem lines="full">
+                        <IonIcon slot="start" className="ion-margin-end" icon={todayOutline}></IonIcon>
+                        <IonLabel>
+                          <p>Date of birth</p>
+                          <h2 className="font-weight-bold">{new Intl.DateTimeFormat('en-GB').format(new Date(driver.DriverStandings[0].Driver.dateOfBirth))}</h2>
+                        </IonLabel>
+                      </IonItem>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonSlide>
+            </IonSlides>
           </IonContent>
         )}
     </IonPage>
