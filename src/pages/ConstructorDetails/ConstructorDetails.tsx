484bcc5de59ba43486d25411dd4cd7ee94ff3344
dmartinjs <53537199+dmartinjs@users.noreply.github.com>
2023-02-05 11:59:55 +0100

    feat(ConstructorDetails): add season list

--- src/pages/ConstructorDetails/ConstructorDetails.tsx
+++ src/pages/ConstructorDetails/ConstructorDetails.tsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonToolbar, IonButtons, IonBackButton, IonItem, IonLabel, IonThumbnail, IonIcon, IonImg, IonSegment, IonSegmentButton, IonSlides, IonSlide, IonGrid, IonRow, IonCol } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonToolbar, IonButtons, IonBackButton, IonItem, IonLabel, IonThumbnail, IonIcon, IonImg, IonSegment, IonSegmentButton, IonSlides, IonSlide, IonGrid, IonRow, IonCol, IonBadge } from '@ionic/react';
 import { RouteComponentProps } from 'react-router';
-import { ConstructorStandingsLists, Driver } from '../../models';
+import { ConstructorStandingsLists } from '../../models';
 import './ConstructorDetails.css';
 import { slideOptions } from '../../utils/SlideOptions';
 import { readerOutline, todayOutline } from 'ionicons/icons';
 
 interface ConstructorDetailsProps extends RouteComponentProps<{
   constructorId: string,
-}> {}
+}> { }
 
-const ConstructorDetails: React.FC<ConstructorDetailsProps> = ({match}) => {
-  const [constructor, setConstructor] = useState<ConstructorStandingsLists | null>(null);
-  const [drivers, setDrivers] = useState<[Driver] | null>(null);
+const ConstructorDetails: React.FC<ConstructorDetailsProps> = ({ match }) => {
+  const [constructor, setConstructor] = useState<[ConstructorStandingsLists] | null>(null);
   const [season, setSeason] = useState<number | null>(null);
   const [description, setDescription] = useState<string | null>(null);
   const [selectedSegment, SetSelectedSegment] = useState<string>('stats');
@@ -22,11 +21,11 @@ const ConstructorDetails: React.FC<ConstructorDetailsProps> = ({match}) => {
   const onSegmentChange = (event: CustomEvent) => {
     SetSelectedSegment(event.detail.value);
 
-    switch(event.detail.value) {
+    switch (event.detail.value) {
       case 'stats':
         slider.current!.slideTo(0);
         break;
-      case 'drivers':
+      case 'seasons':
         slider.current!.slideTo(1);
         break;
       case 'bio':
@@ -37,12 +36,12 @@ const ConstructorDetails: React.FC<ConstructorDetailsProps> = ({match}) => {
 
   const onSlideChange = (event: any) => {
     event.target.getActiveIndex().then((value: any) => {
-      switch(value) {
+      switch (value) {
         case 0:
           SetSelectedSegment('stats');
           break;
         case 1:
-          SetSelectedSegment('drivers');
+          SetSelectedSegment('seasons');
           break;
         case 2:
           SetSelectedSegment('bio');
@@ -52,7 +51,7 @@ const ConstructorDetails: React.FC<ConstructorDetailsProps> = ({match}) => {
   }
 
   useEffect(() => {
-    fetch(`https://ergast.com/api/f1/current/constructors/${match.params.constructorId}/constructorStandings.json`)
+    fetch(`https://ergast.com/api/f1/constructors/${match.params.constructorId}/constructorStandings.json?limit=500`)
       .then(res => res.json())
       .then(result => {
 
@@ -63,16 +62,8 @@ const ConstructorDetails: React.FC<ConstructorDetailsProps> = ({match}) => {
             setDescription((xmlDoc.querySelector("extract") as HTMLElement).textContent);
           });
 
-        return setConstructor(result.MRData.StandingsTable.StandingsLists[0]);
+        return setConstructor(result.MRData.StandingsTable.StandingsLists.reverse());
       });
-
-    fetch(`https://ergast.com/api/f1/current/constructors/${match.params.constructorId}/drivers.json`)
-      .then(res => res.json())
-      .then(result => setDrivers(result.MRData.DriverTable.Drivers));
-
-    fetch(`https://ergast.com/api/f1/constructors/${match.params.constructorId}/seasons.json`)
-      .then(res => res.json())
-      .then(result => setSeason(result.MRData.SeasonTable.Seasons[0].season));
   }, [match.params.constructorId]);
 
   return (
@@ -84,25 +75,25 @@ const ConstructorDetails: React.FC<ConstructorDetailsProps> = ({match}) => {
           </IonButtons>
         </IonToolbar>
         <IonToolbar>
-        {constructor && (
-          <IonItem className='toolbar-item'>
-            <IonIcon lazy slot="start" size="large" className="constructor ion-margin-end" src={`assets/img/constructors/${constructor.ConstructorStandings[0].Constructor.constructorId}.svg`}/>
-            <IonLabel>
-              <h2 className="font-weight-bold">{constructor.ConstructorStandings[0].Constructor.name}</h2>
-            </IonLabel>
-            <IonThumbnail slot="end" className="country-thumbnail">
-              <IonImg src={`assets/img/flags/${constructor.ConstructorStandings[0].Constructor.nationality}.svg`} alt={constructor.ConstructorStandings[0].Constructor.nationality}/>
-            </IonThumbnail>
-          </IonItem>
-        )}
+          {constructor && (
+            <IonItem className='toolbar-item'>
+              <IonIcon lazy slot="start" size="large" className="constructor ion-margin-end" src={`assets/img/constructors/${constructor[0].ConstructorStandings[0].Constructor.constructorId}.svg`} />
+              <IonLabel>
+                <h2 className="font-weight-bold">{constructor[0].ConstructorStandings[0].Constructor.name}</h2>
+              </IonLabel>
+              <IonThumbnail slot="end" className="country-thumbnail">
+                <IonImg src={`assets/img/flags/${constructor[0].ConstructorStandings[0].Constructor.nationality}.svg`} alt={constructor[0].ConstructorStandings[0].Constructor.nationality} />
+              </IonThumbnail>
+            </IonItem>
+          )}
         </IonToolbar>
         <IonToolbar>
           <IonSegment onIonChange={onSegmentChange} value={selectedSegment}>
             <IonSegmentButton value="stats">
               <IonLabel>Stats</IonLabel>
             </IonSegmentButton>
-            <IonSegmentButton value="drivers">
-              <IonLabel>Drivers</IonLabel>
+            <IonSegmentButton value="seasons">
+              <IonLabel>Seasons</IonLabel>
             </IonSegmentButton>
             <IonSegmentButton value="bio">
               <IonLabel>Bio</IonLabel>
@@ -124,21 +115,19 @@ const ConstructorDetails: React.FC<ConstructorDetailsProps> = ({match}) => {
           <IonSlide>
             <IonGrid>
               <IonRow>
-                {drivers && constructor && drivers.slice(0, 2).map(driver =>
-                  <IonCol>
-                    <IonRow>
-                      <IonCol>
-                        <IonItem button lines='full' routerLink={`/driver/${driver.driverId}`} key={driver.driverId}>
-                          <div slot="start" className={`driver-number driver-details-number ion-margin-end driver-${constructor.ConstructorStandings[0].Constructor.constructorId}`}>{driver.permanentNumber}</div>
-                          <IonLabel>
-                            <p>{driver.givenName}</p>
-                            <h2 className="font-weight-bold ion-text-uppercase">{driver.familyName}</h2>
-                          </IonLabel>
-                        </IonItem>
-                      </IonCol>
-                    </IonRow>
-                  </IonCol>
-                )}
+                <IonCol>
+                  {constructor && constructor.map(season => 
+                    <IonItem key={season.season} lines="full" button>
+                      <IonLabel>
+                        <p>{season.season}</p>
+                      </IonLabel>
+                      <div className="race-position ion-margin-end font-weight-bold">
+                        {season.ConstructorStandings[0].position}
+                      </div>
+                      <IonBadge className="standings-points" slot="end" color="medium" mode="ios">{season.ConstructorStandings[0].points}</IonBadge>
+                    </IonItem>
+                  )}
+                </IonCol>
               </IonRow>
             </IonGrid>
           </IonSlide>
@@ -150,18 +139,18 @@ const ConstructorDetails: React.FC<ConstructorDetailsProps> = ({match}) => {
                     <>
                       <IonItem lines="full">
                         <IonThumbnail slot="start" className="country-thumbnail ion-margin-end">
-                          <IonImg src={`assets/img/flags/${constructor.ConstructorStandings[0].Constructor.nationality}.svg`} alt={constructor.ConstructorStandings[0].Constructor.nationality}/>
+                          <IonImg src={`assets/img/flags/${constructor[0].ConstructorStandings[0].Constructor.nationality}.svg`} alt={constructor[0].ConstructorStandings[0].Constructor.nationality} />
                         </IonThumbnail>
                         <IonLabel>
                           <p>Nationality</p>
-                          <h2 className="font-weight-bold">{constructor.ConstructorStandings[0].Constructor.nationality}</h2>
+                          <h2 className="font-weight-bold">{constructor[0].ConstructorStandings[0].Constructor.nationality}</h2>
                         </IonLabel>
                       </IonItem>
                       <IonItem>
                         <IonIcon slot="start" className="ion-align-self-start ion-margin-end" icon={todayOutline}></IonIcon>
                         <IonLabel className='preline'>
                           <p>First Team Entry</p>
-                          <h2>{season}</h2>
+                          <h2>{constructor[constructor.length -1].season}</h2>
                         </IonLabel>
                       </IonItem>
                       <IonItem>
